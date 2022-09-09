bc02dd960b2cf0c6903d596e0d73f825467fa75d
xCheis <45414411+xCheis@users.noreply.github.com>
2022-09-08 22:41:31 -0500

    Update index.js
    change service

--- index.js
+++ index.js
@@ -10,40 +10,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-let data = [
-    {
-        "SAT_MA_ID_SERVICIO": "2020E000001",
-        "EFFDT": "2020-06-26T16:54:15.000Z",
-        "SAT_MA_TIPOSERV_X": "CURP",
-        "SAT_MA_NOMSERVIC": "Consulta de Declaraciones y Pagos Web",
-        "SAT_MA_STATUS_X": "A",
-        "DESCR254": "CUN-WEB 2",
-        "BGN_DT": "2008-01-01T06:00:00.000Z",
-        "END_DT": null
-    },
-    {
-        "SAT_MA_ID_SERVICIO": "2020E000001",
-        "EFFDT": "2020-06-26T16:54:15.000Z",
-        "SAT_MA_TIPOSERV_X": "CURP",
-        "SAT_MA_NOMSERVIC": "Consulta de Declaraciones y Pagos Web",
-        "SAT_MA_STATUS_X": "A",
-        "DESCR254": "CUN-WEB 2",
-        "BGN_DT": "2008-01-01T06:00:00.000Z",
-        "END_DT": null
-    },
-    {
-        "SAT_MA_ID_SERVICIO": "2020E000001",
-        "EFFDT": "2020-06-26T16:54:15.000Z",
-        "SAT_MA_TIPOSERV_X": "CURP",
-        "SAT_MA_NOMSERVIC": "Consulta de Declaraciones y Pagos Web",
-        "SAT_MA_STATUS_X": "A",
-        "DESCR254": "CUN-WEB 2",
-        "BGN_DT": "2008-01-01T06:00:00.000Z",
-        "END_DT": null
-    }
-]
-
-
 
 app.get('/', (req, res) => {
     res.json({
@@ -55,7 +21,7 @@ app.get('/', (req, res) => {
 
 app.post('/cncatalogos', async function(req, res) {
 
-    res.json(data);
+    res.json(req.header);
 
 
 });
@@ -63,5 +29,4 @@ app.post('/cncatalogos', async function(req, res) {
 
 app.listen(port,  function(req, res) {
     console.log('El sitio de APIs inició correctamente en el puerto: ', port);
-    res;
 });
